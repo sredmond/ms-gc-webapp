07c201d4ccc0a302484cd4f5354717b3235f4f19
Sam Redmond <sam.redmond@menloschool.org>
2013-08-14 23:46:44 -0700

    Resets routes.py to something better

--- app/routes.py
+++ app/routes.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-  print "Booyahcashoaw KIRK"
   return render_template('home.html')
 
 @app.route('/about')
 def about():
-  print "KIRK IS GREAT"
   return render_template('about.html')
 
 if __name__ == '__main__':
